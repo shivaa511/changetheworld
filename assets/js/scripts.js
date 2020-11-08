802e5556d52e1ff418ffa17eaaa646530766dc62
ligo3654 <siva.bathala@uni-weimar.de>
2020-11-08 22:18:32 +0100

    changes
    changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -411,3 +411,11 @@ jQuery(document).ready(function() {
     });
 
 });
+function isNumber(evt)
+		 {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true; }
